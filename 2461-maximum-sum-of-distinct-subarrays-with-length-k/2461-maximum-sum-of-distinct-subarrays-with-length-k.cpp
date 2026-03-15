802da98824d825bdbde8cf2bce9cd20aa08ade69
Dilip Kumar <dilipkumar.dev.official@gmail.com>
2026-03-15 16:48:54 +0530

    Time: 239 ms (5.38%), Space: 113.3 MB (5.17%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,23 +1,23 @@
-
-#include <set>
+#include <algorithm>
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long currSum = 0, maxSum = 0; 
-        int left = 0;
-        set<int> store;
-        for (int right = 0; right < nums.size(); right++){
-            while (store.count(nums[right]) || store.size() == k){
+        set<int> subSet;
+        long long currSum = 0;
+        long long maxSum = 0;
+        int left;
+        for(int right = 0; right < nums.size(); right++){
+            while (subSet.contains(nums[right]) || subSet.size() == k){
+                subSet.erase(nums[left]);
                 currSum -= nums[left];
-                store.erase(nums[left]);
                 left++;
             }
             currSum += nums[right];
-            store.insert(nums[right]);
-            if (store.size() == k){
+            subSet.insert(nums[right]);
+            if (subSet.size() == k){
                 maxSum = max(currSum, maxSum);
             }
-        }
+        }   
         return maxSum;
     }
 };

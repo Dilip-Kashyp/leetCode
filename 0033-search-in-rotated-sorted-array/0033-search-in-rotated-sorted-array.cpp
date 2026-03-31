c4882526b44f9d1a8bb014c288d6b03037eb647e
Dilip Kumar <dilipkumar.dev.official@gmail.com>
2026-03-31 18:22:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,20 +8,19 @@ public:
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] >= nums[left]) {
-                if ( nums[left] <= target && nums[mid] >= target) {
+                if (nums[left] <= target && nums[mid] >= target) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
-                } 
+                }
+            } else {
+                if (nums[mid] <= target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
-            else {
-                    if (  nums[mid] <= target && nums[right] >= target ) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    } 
-            }
-        } 
-        return - 1;
+        }
+        return -1;
     }
 };

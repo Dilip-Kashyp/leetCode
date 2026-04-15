17c2b7bc4777f9a43a033005839f166d6b3ddfed
Dilip Kumar <dilipkumar.dev.official@gmail.com>
2026-04-15 11:56:56 +0530

    Time: 7 ms (10.57%), Space: 12.4 MB (84.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,10 +6,10 @@ public:
         for (int num1 : nums1) {
             int nextGreater = -1;
             bool found = false;
-            for (int i = 0; i < nums2.size()-1; i++) {
-                if ( num1 == nums2[i]) {
+            for (int i = 0; i < nums2.size(); i++) {
+                if ( nums2[i] == num1 ) {
                     found = true;
-                } else if ( found && nums2[i] > num1 ) {
+                } else if (found && nums2[i] > num1 ) {
                     nextGreater = nums2[i];
                     break;
                 }

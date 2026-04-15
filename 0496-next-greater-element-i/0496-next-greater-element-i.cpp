ffb5bfa030a7f072710805d791f2dd4c5c9d8b95
Dilip Kumar <dilipkumar.dev.official@gmail.com>
2026-04-15 11:54:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
-
+        vector <int> result;
+        
         for (int num1 : nums1) {
             int nextGreater = -1;
             bool found = false;
-
-            for (int i = 0; i < nums2.size(); i++) {
-                if (nums2[i] == num1) {
+            for (int i = 0; i < nums2.size()-1; i++) {
+                if ( num1 == nums2[i]) {
                     found = true;
-                } else if (found && nums2[i] > num1) {
+                } else if ( found && nums2[i] > num1 ) {
                     nextGreater = nums2[i];
                     break;
                 }
             }
-
             result.push_back(nextGreater);
         }
         return result;
